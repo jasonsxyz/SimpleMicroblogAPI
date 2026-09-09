26131ac54895154d903f327b399ee203c93c2b35
Jason <jasonsxyz@protonmail.com>
2026-09-09 19:49:39 +1000

    posts and comments

--- db_crud.py
+++ db_crud.py
@@ -47,6 +47,26 @@ def find_user_from_id(user_id):
     finally:
         conn.close()
 
+def get_post(post_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * From Post WHERE post_id = ?", (post_id,))
+        return cursor.fetchone()
+    finally:
+        conn.close()
+
+def get_comments(post_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * From Comment WHERE post_id = ? ORDER BY created_at DESC", (post_id,))
+        return cursor.fetchall()
+    finally:
+        conn.close()
+
 def get_posts(amount, user_id):
     conn = get_connection()
     cursor = conn.cursor()
@@ -67,6 +87,10 @@ def get_posts(amount, user_id):
 def create_post(name, content):
     conn = get_connection()
     cursor = conn.cursor()
+
+    if not find_user(name):
+        create_user(name)
+    
     user_id = find_user(name)[0]
 
     try:
@@ -79,3 +103,21 @@ def create_post(name, content):
     finally:
         conn.close()
 
+def create_comment(name, content, post_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if not find_user(name):
+        create_user(name)
+    
+    user_id = find_user(name)[0]
+
+    try:
+        cursor.execute("""
+        INSERT INTO Comment (post_id, user_id, content) 
+        VALUES (?, ?, ?)
+        """, (post_id, user_id, content))
+        conn.commit()
+        return cursor.fetchone()
+    finally:
+        conn.close()
--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from flask import Flask, jsonify, request, abort
 from db_crud import *
 app = Flask(__name__)
 
+# get 
 @app.route('/api/posts', methods=['GET'])
 def fetch_posts():
     from_user = request.args.get('user')
+    select = request.args.get('select')
+
+    if select:
+        if get_post(select):
+            return jsonify({"post": get_post(select)}, {"comments": get_comments(select)})
 
     if not from_user:
         posts = get_posts(50, None)
@@ -22,7 +28,30 @@ def fetch_user():
     if not find_user_from_id(user_id):
         abort(404)
 
-    return jsonify(find_user_from_id(user_id))
+    return jsonify({"user": find_user_from_id(user_id)})
+
+# post
+@app.route('/api/post', methods=['POST'])
+def post():
+    data = request.json
+    content = data.get('content')
+    user = data.get('user')
+    post = data.get('post_id')
+
+    if post:
+        if get_post(post):
+            create_comment(user, content, post)
+            return jsonify({"status": "success", "message": "Post created"}), 201
+        else:
+            return jsonify({"post_id": post})
+
+
+    if not content or not user:
+        abort(400)
+
+    create_post(user, content)
+
+    return jsonify({"status": "success", "message": "Post created"}), 201
 
 
 app.run(debug=True, port=5000)
